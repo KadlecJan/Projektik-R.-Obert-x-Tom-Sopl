3899b6e14a6a6559b18d7cdc6c9d394d01b86b8d
TomasSobl <soblt22@spstrutnov.cz>
2025-10-06 10:42:53 +0200

    addPostForm addPost

--- app/api/category.db.js
+++ app/api/category.db.js
@@ -22,3 +22,13 @@ export async function getPostsInCategory(slug) {
 
   return response;
 }
+
+export async function addPost(slug, date, autor, title, text, category, img) {
+  const response = await sql(
+    `INSERT INTO projektik_articles (slug, date, autor, title, text, category, img) VALUES ('${slug}', NOW(),'${autor}','${title}','${text}','${category}','${img}')`
+  );
+  if (!response) {
+    console.error("No response from SQL function");
+    return null;
+  }
+}
